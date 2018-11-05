f057178907ce1ab2f8f210e93f1fddcc797942ce
Deniz Safak Tutuncu <denizsafaktutuncu@gmail.com>
2018-11-05 13:20:02 -0800

    Convert to MVC design pattern.

---
+++ Todoey/data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Deniz Tutuncu on 11/5/18.
+//  Copyright © 2018 Deniz Tutuncu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
